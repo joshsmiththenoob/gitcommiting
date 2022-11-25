a9385576b4b22b2450209a2b1c301ab9e1ff3fc8
0932409832094 <joshsmith0223029@gmail.com>
2022-11-25 14:06:23 +0800

    This is the Test Description for repo

---
+++ test.py
@@ -0,0 +1,21 @@
+### This is the Beginning of Tibame Course ###
+# 註解的內容"說明程式的功能
+print("你需要的運算子:")
+x=input()
+if x == "+":
+    a=10+2
+    print(a)
+elif x == "-":
+    a = 10 - 2
+    print(a)
+elif x == "*":
+    a = 10 * 2
+    print(a)
+elif x == "/":
+    a = 10 / 2
+    print(a)
+else:
+    print("這蝦雞巴的符號打哪來的")
+
+
+
